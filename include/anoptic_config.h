6b10cdf2632663d923dddb5a8df1bebd67fb7e07
cris <primaryuser@gmx.com>
2023-10-03 15:02:21 +0100

    Extended device selection logic to prefer dGPUs

--- include/anoptic_config.h
+++ include/anoptic_config.h
@@ -1,6 +1,7 @@
 /* SPDX-FileCopyrightText: 2023 Anoptic Game Engine Authors
  *
  * SPDX-License-Identifier: LGPL-3.0 */
+/*  == Anoptic Game Engine v0.0000001 == */
 
 /**
  * @file anoptic_config.h
@@ -15,5 +16,5 @@
 
  
 
- #endif // ANOPTIC_MEMORY_H
+ #endif // ANOPTIC_CONFIG_H
  // end of include guard, end of file
