1505f5c823ecc02eef3a661bec84e03b17a973c8
matei3d <matei3d@gmail.com>
2023-11-18 15:46:13 -0500

    Corrected threading interface.

--- include/anoptic_threads.h
+++ include/anoptic_threads.h
@@ -10,133 +10,133 @@
 #include <stddef.h>
 #include <pthread.h>
 
-/// \brief Represents a thread handle.
-typedef pthread_t ano_thread_t;
+// Represents a thread handle.
+typedef pthread_t anothread_t;
 
-/// \brief Represents a mutex handle.
-typedef pthread_mutex_t ano_mutex_t;
+// Represents a mutex handle.
+typedef pthread_mutex_t anothread_mutex_t;
 
-/// \brief Function signature for thread entry point.
-typedef void *(*ano_thread_func)(void *arg);
+// Represents a thread attribute
+typedef pthread_attr_t anothread_attr_t;
+
+// Represents condition variables and attributes
+typedef pthread_cond_t anothread_cond_t;
+typedef pthread_condattr_t anothread_condattr_t;
+typedef pthread_attr_t anothread_attr_t;
+
+// Represents spinlock types
+typedef pthread_spinlock_t anothread_spinlock_t;
+
+// Represents rwlock types.
+typedef pthread_rwlock_t anothread_rwlock_t;
+typedef pthread_rwlockattr_t anothread_rwlockattr_t;
+
+// Represents barrier types
+typedef pthread_barrier_t anothread_barrier_t;
+typedef pthread_barrierattr_t anothread_barrierattr_t;
+
+// Represents key types for thread local storage
+typedef pthread_key_t anothread_key_t;
+
+
+// Function signature for thread entry point.
+//typedef void *(*ano_thread_func)(void *arg);
 
 /* Thread Management */
 
-/// \brief Creates a new thread.
-/// \param func The function to be executed by the thread.
-/// \param arg The arguments to be passed to the thread function.
-/// \param thread A pointer to the thread handle.
-/// \return 0 on success, non-zero on failure.
-int ano_thread_create(ano_thread_func func, void *arg, ano_thread_t **thread);
+int ano_thread_create(anothread_t *thread, const anothread_attr_t *attr, void *(* func)(void *), void *arg);
 
-/// \brief Joins a thread, waiting for it to finish execution.
-/// \param thread The thread handle.
-/// \return 0 on success, non-zero on failure.
-int ano_thread_join(ano_thread_t *thread);
+int ano_thread_join(anothread_t thread, void **res);
 
-/// \brief Terminates the calling thread.
-/// \param thread The thread handle.
-/// \return 0 on success, non-zero on failure.
-int ano_thread_exit(ano_thread_t *thread);
+int ano_thread_exit(void *res);
 
-int ano_thread_detach(pthread_t thread);
+int ano_thread_detach(anothread_t thread);
 
-pthread_t ano_thread_self(void);
+anothread_t ano_thread_self(void);
 
 
 /* Mutexes */
 
-/// \brief Initializes a mutex.
-/// \param mutex A pointer to the mutex handle.
-/// \return 0 on success, non-zero on failure.
-int ano_mutex_init(ano_mutex_t **mutex);
+int ano_mutex_init(anothread_mutex_t **mutex);
 
-/// \brief Locks a mutex.
-/// \param mutex The mutex handle.
-/// \return 0 on success, non-zero on failure.
-int ano_mutex_lock(ano_mutex_t *mutex);
+int ano_mutex_lock(anothread_mutex_t *mutex);
 
-/// \brief Unlocks a mutex.
-/// \param mutex The mutex handle.
-/// \return 0 on success, non-zero on failure.
-int ano_mutex_unlock(ano_mutex_t *mutex);
+int ano_mutex_unlock(anothread_mutex_t *mutex);
 
-/// \brief Destroys a mutex.
-/// \param mutex The mutex handle.
-/// \return 0 on success, non-zero on failure.
-int ano_mutex_destroy(ano_mutex_t *mutex);
+int ano_mutex_destroy(anothread_mutex_t *mutex);
 
 
 /* Condition Variables */
-int ano_thread_cond_init(pthread_cond_t *conditionVariable, const pthread_condattr_t *attribute);
+int ano_thread_cond_init(anothread_cond_t *conditionVariable, const anothread_condattr_t *attribute);
 
-int ano_thread_cond_wait(pthread_cond_t *conditionVariable, pthread_mutex_t *external_mutex);
+int ano_thread_cond_wait(anothread_cond_t *conditionVariable, anothread_mutex_t *external_mutex);
 
-int  ano_thread_cond_signal(pthread_cond_t *conditionVariable);
+int  ano_thread_cond_signal(anothread_cond_t *conditionVariable);
 
-int ano_thread_cond_broadcast(pthread_cond_t *conditionVariable);
+int ano_thread_cond_broadcast(anothread_cond_t *conditionVariable);
 
-int ano_thread_cond_destroy(pthread_cond_t *conditionVariable);
+int ano_thread_cond_destroy(anothread_cond_t *conditionVariable);
 
 
 /* Spinlocks */
-int ano_thread_spin_init(pthread_spinlock_t *lock, int pshared);
+int ano_thread_spin_init(anothread_spinlock_t *lock, int pshared);
 
-int ano_thread_spin_destroy(pthread_spinlock_t *lock);
+int ano_thread_spin_destroy(anothread_spinlock_t *lock);
 
-int ano_thread_spin_lock(pthread_spinlock_t *lock);
+int ano_thread_spin_lock(anothread_spinlock_t *lock);
 
-int ano_thread_spin_trylock(pthread_spinlock_t *lock);
+int ano_thread_spin_trylock(anothread_spinlock_t *lock);
 
-int ano_thread_spin_unlock(pthread_spinlock_t *lock);
+int ano_thread_spin_unlock(anothread_spinlock_t *lock);
 
 
 /* Read-Write Locks */
 
-int ano_thread_rwlock_init(pthread_rwlock_t *rwlock);
+int ano_thread_rwlock_init(anothread_rwlock_t *rwlock, const anothread_rwlockattr_t *attr);
 
-int ano_thread_rwlock_rdlock(pthread_rwlock_t *rwlock);
+int ano_thread_rwlock_rdlock(anothread_rwlock_t *rwlock);
 
-int ano_thread_rwlock_wrlock(pthread_rwlock_t *rwlock);
+int ano_thread_rwlock_wrlock(anothread_rwlock_t *rwlock);
 
-int ano_thread_rwlock_unlock(pthread_rwlock_t *rwlock);
+int ano_thread_rwlock_unlock(anothread_rwlock_t *rwlock);
 
-int ano_thread_rwlock_destroy(pthread_rwlock_t *rwlock);
+int ano_thread_rwlock_destroy(anothread_rwlock_t *rwlock);
 
 
 /* Thread Attributes */
 
-int ano_thread_attr_init(pthread_condattr_t *attribute);
+int ano_thread_attr_init(anothread_condattr_t *attribute);
 
-int ano_thread_attr_setdetachstate(pthread_attr_t *attribute, int flag);
+int ano_thread_attr_setdetachstate(anothread_attr_t *attribute, int flag);
 
-int ano_thread_attr_setstacksize(pthread_attr_t *attribute, size_t *size);
+int ano_thread_attr_setstacksize(anothread_attr_t *attribute, size_t *size);
 
-int ano_thread_attr_destroy(pthread_attr_t *attr);
+int ano_thread_attr_destroy(anothread_attr_t *attr);
 
 
 /* Thread-Data */
 
-int ano_thread_key_create(pthread_key_t *key, void (*destructor)(void *));
+int ano_thread_key_create(anothread_key_t *key, void (*destructor)(void *));
 
-int ano_thread_key_delete(pthread_key_t key);
+int ano_thread_key_delete(anothread_key_t key);
 
-int ano_thread_setspecific(pthread_key_t key, const void *value);
+int ano_thread_setspecific(anothread_key_t key, const void *value);
 
-int ano_thread_getspecific(pthread_key_t key);
+int ano_thread_getspecific(anothread_key_t key);
 
 
 /* Synchronization Barriers */
 
-int ano_thread_barrier_init(pthread_barrier_t *barrier, const pthread_barrierattr_t *attr, unsigned int count);
+int ano_thread_barrier_init(anothread_barrier_t *barrier, const anothread_barrierattr_t *attr, unsigned int count);
 
-int ano_thread_barrier_wait(pthread_barrier_t *barrier);
+int ano_thread_barrier_wait(anothread_barrier_t *barrier);
 
-int ano_thread_barrier_destroy(pthread_barrier_t *barrier);
+int ano_thread_barrier_destroy(anothread_barrier_t *barrier);
 
 
 /* Thread Cancellation */
 
-int ano_thread_cancel(pthread_t thread);
+int ano_thread_cancel(anothread_t thread);
 
 int ano_thread_setcancelstate(int state, int *oldstate);
 
--- src/threads/threads.c
+++ src/threads/threads.c
@@ -1,5 +1,222 @@
-//
-// Created by Pyrus on 2023-10-14.
-//
+/* SPDX-FileCopyrightText: 2023 Anoptic Game Engine Authors
+ *
+ * SPDX-License-Identifier: LGPL-3.0 */
+/*  == Anoptic Game Engine v0.0000001 == */
 
-// meow test
+#include <anoptic_threads.h>
+#include <pthread.h>
+
+
+/* Thread Management */
+
+int ano_thread_create(anothread_t *thread, const anothread_attr_t *attr, void *(* func)(void *), void *arg) {
+
+    return 0;
+}
+
+int ano_thread_join(anothread_t thread, void **res) {
+
+    return 0;
+}
+
+int ano_thread_exit(void *res) {
+
+    return 0;
+}
+
+int ano_thread_detach(anothread_t thread) {
+
+    return 0;
+}
+
+anothread_t ano_thread_self(void) {
+
+    return 0;
+}
+
+
+/* Mutexes */
+
+int ano_mutex_init(anothread_mutex_t **mutex) {
+
+    return 0;
+}
+
+int ano_mutex_lock(anothread_mutex_t *mutex) {
+
+    return 0;
+}
+
+int ano_mutex_unlock(anothread_mutex_t *mutex) {
+
+    return 0;
+}
+
+int ano_mutex_destroy(anothread_mutex_t *mutex) {
+
+    return 0;
+}
+
+
+/* Condition Variables */
+int ano_thread_cond_init(anothread_cond_t *conditionVariable, const anothread_condattr_t *attribute) {
+
+    return 0;
+}
+
+int ano_thread_cond_wait(anothread_cond_t *conditionVariable, anothread_mutex_t *external_mutex) {
+
+    return 0;
+}
+
+int  ano_thread_cond_signal(anothread_cond_t *conditionVariable) {
+
+    return 0;
+}
+
+int ano_thread_cond_broadcast(anothread_cond_t *conditionVariable) {
+
+    return 0;
+}
+
+int ano_thread_cond_destroy(anothread_cond_t *conditionVariable) {
+
+    return 0;
+}
+
+
+/* Spinlocks */
+int ano_thread_spin_init(anothread_spinlock_t *lock, int pshared) {
+
+    return 0;
+}
+
+int ano_thread_spin_destroy(anothread_spinlock_t *lock) {
+
+    return 0;
+}
+
+int ano_thread_spin_lock(anothread_spinlock_t *lock) {
+
+    return 0;
+}
+
+int ano_thread_spin_trylock(anothread_spinlock_t *lock) {
+
+    return 0;
+}
+
+int ano_thread_spin_unlock(anothread_spinlock_t *lock) {
+
+    return 0;
+}
+
+
+/* Read-Write Locks */
+
+int ano_thread_rwlock_init(anothread_rwlock_t *rwlock, const anothread_rwlockattr_t *attr) {
+
+    return 0;
+}
+
+int ano_thread_rwlock_rdlock(anothread_rwlock_t *rwlock) {
+
+    return 0;
+}
+
+int ano_thread_rwlock_wrlock(anothread_rwlock_t *rwlock) {
+
+    return 0;
+}
+
+int ano_thread_rwlock_unlock(anothread_rwlock_t *rwlock) {
+
+    return 0;
+}
+
+int ano_thread_rwlock_destroy(anothread_rwlock_t *rwlock) {
+
+    return 0;
+}
+
+
+/* Thread Attributes */
+
+int ano_thread_attr_init(anothread_condattr_t *attribute) {
+
+    return 0;
+}
+
+int ano_thread_attr_setdetachstate(anothread_attr_t *attribute, int flag) {
+
+    return 0;
+}
+
+int ano_thread_attr_setstacksize(anothread_attr_t *attribute, size_t *size) {
+
+    return 0;
+}
+
+int ano_thread_attr_destroy(anothread_attr_t *attr) {
+
+    return 0;
+}
+
+
+/* Thread-Data */
+
+int ano_thread_key_create(anothread_key_t *key, void (*destructor)(void *)) {
+
+    return 0;
+}
+
+int ano_thread_key_delete(anothread_key_t key) {
+
+    return 0;
+}
+
+int ano_thread_setspecific(anothread_key_t key, const void *value) {
+
+    return 0;
+}
+
+int ano_thread_getspecific(anothread_key_t key) {
+
+    return 0;
+}
+
+
+/* Synchronization Barriers */
+
+int ano_thread_barrier_init(anothread_barrier_t *barrier, const anothread_barrierattr_t *attr, unsigned int count) {
+
+    return 0;
+}
+
+int ano_thread_barrier_wait(anothread_barrier_t *barrier) {
+
+    return 0;
+}
+
+int ano_thread_barrier_destroy(anothread_barrier_t *barrier) {
+
+    return 0;
+}
+
+
+/* Thread Cancellation */
+
+int ano_thread_cancel(anothread_t thread) {
+
+    return 0;
+}
+
+int ano_thread_setcancelstate(int state, int *oldstate) {
+
+    return 0;
+}
+
+int ano_thread_setcanceltype(int type, int *oldtype) {
+
+    return 0;
+}
