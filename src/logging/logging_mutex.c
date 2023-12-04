2c19f63841ef16ff25e3f579d14786801dcd9ef5
peponpylon <pylondev@protonmail.com>
2023-12-04 11:27:02 -0500

    Logging mutex version.

--- src/logging/logging_mutex.c
+++ src/logging/logging_mutex.c
@@ -8,6 +8,7 @@
 #include <anoptic_threads.h>
 #include <anoptic_time.h>
 #include <anoptic_memalign.h>
+
 #include <stdatomic.h>
 #include <stdbool.h>
 #include <stdarg.h>
@@ -15,43 +16,30 @@
 #include <stdio.h>
 
 // Queues of log messages
-_Alignas(64) static char w_buffer[LOG_BUFFER_SIZE];
-_Alignas(64) static char r_buffer[LOG_BUFFER_SIZE];
-
-static char* write_buffer = w_buffer;
-static char* read_buffer = r_buffer;
+typedef struct {
+    char *log_buffer;
+    _Atomic int tail_index;
+} log_queue_t;
+static log_queue_t write_buffer;
+static anothread_mutex_t write_buffer_mtx;
 
 // Make sure this always matches up to anoptic_logging.h:log_types_t
-static const char* log_strings[] = {"DEBUG", "INFO", "WARN", "ERROR", "FATAL"};
+static const char *log_strings[] = {"DEBUG", "INFO", "WARN", "ERROR", "FATAL"};
 
 static atomic_bool write_flag;
 static atomic_bool swap_flag;
-static _Atomic int tail_index;
 
 static _Atomic int log_write_interval;
 
 /* Module Internal */
-void swap_buffers() {
-
-    swap_flag = 1;
-    while(write_flag) {
-        // wait
-    }
-    char* temp = write_buffer;
-    write_buffer = read_buffer;
-    read_buffer = temp;
-
-    swap_flag = 0;
-}
-
 int flush_log_queue() {
 
-    if (tail_index >= LOG_BUFFER_SIZE) {
+    ano_mutex_lock(&write_buffer_mtx);
+    if (write_buffer.tail_index >= LOG_BUFFER_SIZE) {
         // TODO: Do something
     }
 
-    swap_buffers();
-
+    ano_mutex_unlock(&write_buffer_mtx);
     return 0;
 }
 
@@ -65,7 +53,7 @@ int ano_log_enqueue(log_types_t log_type, const char* fileName, int lineNumber, 
 
     va_list args;
 
-    int maxLen = LOG_BUFFER_SIZE - tail_index;
+    int maxLen = LOG_BUFFER_SIZE - write_buffer.tail_index;
     if (maxLen <= 0) {
         // If buffer is already full, we'll write with log_immediate instead and issue an error.
         ano_log_immediate(LOG_ERROR, __FILE__, __LINE__,
@@ -95,15 +83,13 @@ int ano_log_enqueue(log_types_t log_type, const char* fileName, int lineNumber, 
     int msgLen = snprintf(msgFinal, maxLen, "\n%s %s\n", msgPrefix, msgBody);
 
     // Securing a thread-private sector of the buffer to write within. (Full cache lines)
-    int localIndex;
-    int padding;
-    do {
-        padding = 64 - (msgLen % 64); // end padding after the message body
-        localIndex = tail_index;
-    } while (!atomic_compare_exchange_weak(&tail_index, &localIndex, tail_index + msgLen + padding));
+    ano_mutex_lock(&write_buffer_mtx);
+    int localIndex = write_buffer.tail_index;
+    write_buffer.tail_index += msgLen;
 
     // If the buffer got full, we'll write with log_immediate instead and issue an error.
-    if (tail_index >= LOG_BUFFER_SIZE || localIndex >= LOG_BUFFER_SIZE) {
+    if (write_buffer.tail_index >= LOG_BUFFER_SIZE) {
+        ano_mutex_unlock(&write_buffer_mtx);
         ano_log_immediate(LOG_ERROR, __FILE__, __LINE__,
                           "Log Buffer filled, Writing in immediate mode instead. [Performance Degradation]");
 
@@ -115,16 +101,12 @@ int ano_log_enqueue(log_types_t log_type, const char* fileName, int lineNumber, 
     }
 
     // Inserting *msgFinal into the secured sector of the write_buffer
-    while(swap_flag) {
-        localIndex = 0;
-    }
-    write_flag = 1;
     int i = 0;
-    while(msgFinal[i] != '\0' && localIndex < tail_index) {
-        write_flag = 1;
-        write_buffer[localIndex++] = msgFinal[i++];
+    while(msgFinal[i] != '\0') {
+        write_buffer.log_buffer[localIndex++] = msgFinal[i++];
     }
-    write_flag = 0;
+
+    ano_mutex_unlock(&write_buffer_mtx);
 
     return 0; // Message enqueued successfully.
 }
@@ -138,12 +120,24 @@ void ano_log_immediate(log_types_t log_type, const char* fileName, int lineNumbe
 
 int ano_log_init() {
 
+    if (ano_mutex_init(&write_buffer_mtx, NULL) != 0) {
+        return -1;
+    }
+
+    write_buffer.tail_index = 0;
+    write_buffer.log_buffer = ano_aligned_malloc(LOG_BUFFER_SIZE, 64);
 
 
     return 0;
 }
 
 int ano_log_cleanup() {
+
+    // Destroy the mutex.
+    ano_mutex_destroy(&write_buffer_mtx);
+
+    // Clean up the log buffer.
+    ano_aligned_free(write_buffer.log_buffer);
 
     return 0;
 }
