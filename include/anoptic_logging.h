6ece2efd6481e658913ccb781dc637fa69737098
peponpylon <pylondev@protonmail.com>
2023-12-12 23:01:28 -0500

    Restructured logger to be much cleaner.

--- include/anoptic_logging.h
+++ include/anoptic_logging.h
@@ -8,8 +8,9 @@
 
 #include <stdint.h>
 
-#define LOG_PREFIX_SIZE 256
-#define LOG_BUFFER_SIZE 4096
+#define LOG_PREFIX_MAX 256
+#define LOG_MESSAGE_MAX 4096
+#define LOG_BUFFER_MAX 8192
 
 typedef enum {
     LOG_DEBUG,
@@ -18,11 +19,14 @@ typedef enum {
     LOG_ERROR,
     LOG_FATAL
 } log_types_t;
+static const char *log_strings[] = {"DEBUG", "INFO", "WARN", "ERROR", "FATAL"};
 
 #ifdef DEBUG_BUILD
 #define ano_log_debug(...)  ano_log_enqueue(LOG_DEBUG, __FILE__, __LINE__, __VA_ARGS__)
+#define ano_log_debugnow(...)  ano_log_immediate(LOG_DEBUG, __FILE__, __LINE__, __VA_ARGS__)
 #else
 #define ano_log_debug(...)  /* Debug logging disabled */
+#define ano_log_debugnow(...)  /* Debug logging disabled */
 #endif
 
 #define ano_log_info(...)  ano_log_enqueue(LOG_INFO, __FILE__, __LINE__, __VA_ARGS__)
@@ -39,5 +43,7 @@ int ano_log_init();
 int ano_log_cleanup();
 
 void ano_log_interval(uint32_t ms);
+
+int ano_log_output_dir(const char* directoryPath);
 
 #endif //ANOPTICENGINE_ANOPTIC_LOGGING_H
