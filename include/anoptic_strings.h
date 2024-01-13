77efe6c47b48c270c9fbddfddd1470a31ed304f8
peponpylon <pylondev@protonmail.com>
2024-01-12 21:14:04 -0500

    Anoptic Strings Module stub

---
+++ include/anoptic_strings.h
@@ -0,0 +1,11 @@
+/* SPDX-FileCopyrightText: 2023 Anoptic Game Engine Authors
+ *
+ * SPDX-License-Identifier: LGPL-3.0 */
+/*  == Anoptic Game Engine v0.0000001 == */
+
+#ifndef ANOPTICENGINE_ANOPTIC_STRINGS_H
+#define ANOPTICENGINE_ANOPTIC_STRINGS_H
+
+// Anoptic String API
+
+#endif //ANOPTICENGINE_ANOPTIC_STRINGS_H
