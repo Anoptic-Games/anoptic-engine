e2ad474d5d87cb27c3cd1d8a13651eba8275b13d
MalumCaedoXI <MalumCaedoXI@proton.me>
2024-01-25 00:10:05 +0100

    Trying to add stb_image_write.h

--- src/engine/main.c
+++ src/engine/main.c
@@ -102,10 +102,10 @@ int main()
 	FT_Bitmap* bitmap = ft_get_glyph_bitmap(65);
 	printf("Glyph Width is: %i\n", bitmap->width);
 	printf("Glyph Height is: %i\n", bitmap->rows);
+	ft_debug_save_glyph("Test.bmp", 65);
 	// Create a graphics pipeline
 	
 	//parseGltf("viking_room.gltf");
-
     // Main loop
 	while (!anoShouldClose())
 	{	
--- src/render/text/ano_RenderText.h
+++ src/render/text/ano_RenderText.h
@@ -1,5 +1,6 @@
 #ifndef FT_FREETYPE_H
 #include <ft2build.h>
+#include <stb_image_write.h>
 #include FT_FREETYPE_H
 #endif
 
@@ -7,3 +8,7 @@ int ft_init();
 void ft_add_font(char* file_path, int face_index);
 int ft_load_glyph_bitmap(FT_ULong glyph);
 FT_Bitmap* ft_get_glyph_bitmap(FT_ULong glyph);
+#ifdef DEBUG_BUILD
+
+int ft_debug_save_glyph(char* filename, FT_ULong glyph);
+#endif
