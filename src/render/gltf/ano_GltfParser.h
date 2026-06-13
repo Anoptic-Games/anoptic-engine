328d5da92271d29ea12ee4b410287a08739d18cb
ssa <nextcris@hotmail.com>
2026-06-13 14:51:52 +0100

    Implemented asset blueprint system, untracked .gitignored files.

--- src/render/gltf/ano_GltfParser.h
+++ src/render/gltf/ano_GltfParser.h
@@ -12,7 +12,52 @@
 #include "vulkan_backend/vertex/vertex.h"
 #include "vulkan_backend/instance/instanceInit.h"
 
-// Parses a glTF file, loading assets into GPU memory and setting up renderable entities.
-bool parseGltf(VulkanContext* ctx, const char* fileName);
+// ---------------------------------------------------------
+// Blueprint Assets
+// ---------------------------------------------------------
+
+typedef struct ModelPrimitive {
+    uint32_t geometryPoolIndex;
+    uint32_t materialIndex; // Index into the model's bindless texture handles, etc.
+} ModelPrimitive;
+
+typedef struct ModelMesh {
+    uint32_t primitiveCount;
+    ModelPrimitive* primitives;
+} ModelMesh;
+
+typedef struct ModelNode {
+    char name[64];
+    mat4 localTransform;
+    int32_t meshIndex;   // -1 if no mesh
+    int32_t parentIndex; // -1 if root node
+    uint32_t childCount;
+    uint32_t* childIndices;
+} ModelNode;
+
+// Represents a loaded glTF file resting in GPU memory but NOT spawned in the world
+typedef struct ModelAsset {
+    char name[64];
+    
+    uint32_t nodeCount;
+    ModelNode* nodes;
+    
+    uint32_t meshCount;
+    ModelMesh* meshes;
+    
+    uint32_t rootNodeCount;
+    uint32_t* rootNodes;
+} ModelAsset;
+
+// ---------------------------------------------------------
+// Functions
+// ---------------------------------------------------------
+
+// Parses a glTF file, loading assets into GPU memory and returning a ModelAsset blueprint.
+ModelAsset* parseGltf(VulkanContext* ctx, const char* fileName);
+
+// Spawns a ModelAsset blueprint into the world, returning the root EntityId.
+// For now, it just appends the RenderEntities directly to the RendererState.
+void instantiate_model(ModelAsset* asset, mat4 rootTransform);
 
 #endif
